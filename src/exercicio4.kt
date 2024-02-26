e8094db84903486cd4c4d00910a9c0f259680cca
tat1546 <tatianegm18@gmail.com>
2024-02-26 19:56:33 -0300

    exemplo sala de aula

---
+++ src/exercicio4.kt
@@ -0,0 +1,11 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    // Chamando a função 'imprimirSoma' com os argumentos 4 e 5
+    imprmirSoma(4,5)
+}
+// Definição de uma função chamada 'imprimirSoma' que aceita dois parâmetros do tipo Int
+fun imprmirSoma(a: Int, b: Int) {
+    // Imprime a soma dos dois parâmetros
+    println(a +b)
+}
---
+++ src/exercicio7.kt
@@ -0,0 +1,8 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    // Declaração de uma variável imutável (val) chamada 'bomHumor' com o valor 'true'
+    val bomHumor = true
+    //imprimi uma mensagem
+    print("Hoje estou ${if(bomHumor) "feliz" else "chateado"}")
+}
